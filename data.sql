3ac06dae44316fba052d5ee72cd8d07d59833c14
viktoriaukr <viktoriiauhliai@gmail.com>
2023-10-21 23:42:01 -0500

    updating

--- data.sql
+++ data.sql
@@ -2,6 +2,8 @@
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS branches;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +21,19 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    industry text NOT NULL UNIQUE
+);
+
+
+CREATE TABLE branches (
+    id serial PRIMARY KEY,
+    comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+    industry_code text NOT NULL REFERENCES industries ON DELETE CASCADE
+);
+
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +43,13 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries (code, industry)
+  VALUES ('acct', 'Accounting'),
+         ('it', 'Information Technology'),
+         ('oem', 'Original Equipment Manufacturer.');
+
+
+INSERT INTO branches (comp_code, industry_code)
+  VALUES ('apple', 'oem'),
+         ('ibm', 'oem');
--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ExpressError = require("../expressError");
 const router = new express.Router();
 const db = require("../db");
+const slugify = require("slugify");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -37,7 +38,8 @@ router.get("/:code", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { code, name, description } = req.body;
+    const { name, description } = req.body;
+    const code = slugify(name, { lower: true });
     const results = await db.query(
       `INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description`,
       [code, name, description]
